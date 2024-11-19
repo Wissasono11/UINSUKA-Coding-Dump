f704df8519e0afe2408dcb496fd59f8098ca4156
Bayu Wicaksono <143940265+Wissasono11@users.noreply.github.com>
2024-11-19 08:50:18 +0700

    add: quiz1-oop

--- Quiz OOP /quiz1.php
+++ Quiz OOP /quiz1.php
@@ -1,1 +1,16 @@
+<?php
+    class Orang{
+        private $nama = '';
+        
+        public function setNama($nama){
+            $this->nama = $nama;
+        }
+        
+        public function getNama(){
+            return $this->nama;
+        }
+    }
 
+$orang1 = new Orang();
+$orang1->setNama('Andi');
+echo $orang1->getNama();
