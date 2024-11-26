2acbf6454710f428f5f45d7c407be1f718ca9aa9
Bayu Wicaksono <143940265+Wissasono11@users.noreply.github.com>
2024-11-26 08:28:07 +0700

    Update quiz1.php

--- Quiz-Construct-Destruct-2 /quiz1.php
+++ Quiz-Construct-Destruct-2 /quiz1.php
@@ -1,1 +1,12 @@
-
+<?php
+    class Hewan{
+        private $kucing;
+        
+        public function __construct($kucing){
+            $this->kucing = $kucing;
+        }
+        
+        public function getKucing(){
+            return $this->kucing;
+        }
+    }
