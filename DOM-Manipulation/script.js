3cb6be83b736a2f9139f50d16d3c87c5e4f5df22
Bayu Wicaksono <143940265+Wissasono11@users.noreply.github.com>
2024-11-28 11:55:54 +0700

    Add files via upload

---
+++ DOM-Manipulation/script.js
@@ -0,0 +1,4 @@
+const nodeH1 = document.getElementById("h1");
+nodeH1.style.fontFamily = "Sora, sans-serif";
+nodeH1.style.background = "gold";
+nodeH1.style.textShadow = "5px 5px 5px rgba(0,0,0,0.5)";
