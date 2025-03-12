32e329262a0671d7b9673e7d2292a13a9bcbdbce
Bayu Wicaksono <143940265+Wissasono11@users.noreply.github.com>
2025-03-12 17:24:33 +0700

    Create Gerbong.java

---
+++ Linked List/Gerbong.java
@@ -0,0 +1,9 @@
+public class Gerbong {
+    int data;
+    Gerbong next;
+    
+    public Gerbong(int data){
+        this.data = data;
+        this.next = null; // mula-mula next akan null atau tidak mengait manapun.
+    }
+}
